f6f6b580b4a80590f8552ebd1f94b301176e3a5b
nehachougule1998 <nchougule734@gmail.com>
2022-04-02 12:38:14 +0530

    Added

--- src/com/bridgelabz/MoodAnalyzerMain.java
+++ src/com/bridgelabz/MoodAnalyzerMain.java
@@ -3,19 +3,29 @@ package com.bridgelabz;
 public class MoodAnalyzerMain {
 	
 	private String message;
+	 
+	public MoodAnalyzerMain() {
+		
+	}
 	
 	public MoodAnalyzerMain(String message) {
 		this.message = message;
 	}
-	public String returnEmotion() {
-		if(message == "I am in sad mood") {
+
+ public String returnEmotion() {
+	 try {
+		if(message.equals( "I am in sad mood")) {
 			return "sad";
 		}
-		else if(message == "I am in any mood") {
+		else if(message.equals("I am in any mood")) {
 			return "Happy";
 		}
+		
+	 } catch (NullPointerException e) {
+			System.out.println(e);
 			return "Happy";
 		}
-	}
+	   return message;
+ 	}	
 	
-
+	}
